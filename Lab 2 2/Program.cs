9f5a351f0f757d8fc2b0d3fd61ea093213df2c92
ilyadery <ilyadrbn@gmail.com>
2020-12-14 12:41:51 +0200

    Добавьте файлы проекта.

---
+++ Lab 2 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear(); 
+
+            string f;
+
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.Write("Введите символ: ");
+
+            f = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Введен символ: " + f);
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.Write("Введите значение целого числа: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Введено значение целого числа: " + a);
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.Write("Введите значение вещественного числа: ");
+
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Введено значение вещественного числа: " + "{0:#.###}", b);
+        }
+    }
+}
